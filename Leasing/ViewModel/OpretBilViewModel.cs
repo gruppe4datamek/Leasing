eb0eb7817e7aa0598b79201ad56afa2204a591fa
Danielddwp <48202417+Danielddwp@users.noreply.github.com>
2019-12-06 12:24:20 +0100

    dsf
    sfd

--- Leasing/ViewModel/OpretBilViewModel.cs
+++ Leasing/ViewModel/OpretBilViewModel.cs
@@ -93,8 +93,8 @@ namespace Leasing.ViewModel
 
         public ObservableCollection<Bil> Bils
         {
-            //get { return new ObservableCollection<Bil>(WebApiBilAsync.GetCar("api/Bils/")); }
-            get { return new ObservableCollection<Bil>(); }
+            get { return new ObservableCollection<Bil>(WebApiBilAsync.GetCar("api/Bils/")); }
+            //get { return new ObservableCollection<Bil>(); }
             set { _bils = value; }
         }
 
